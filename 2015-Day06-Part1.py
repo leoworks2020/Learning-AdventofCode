3c3d9eb967083c2d9b27c801def3191ae7aad9f2
leoworks2020 <leoworks2020@gmail.com>
2023-07-10 19:10:33 -0300

    Puzzle 2025-Day06-Part1.py resolved

--- 2015-Day06-Part1.py
+++ 2015-Day06-Part1.py
@@ -21,12 +21,12 @@ turn off 499,499 through 500,500 would turn off (or leave off) the middle four l
 After following the instructions, how many lights are lit?
 --------------------------------------------------------------------------------
 Challenge Start Day:        2023-07-07
-Challenge Completion Day:   x
+Challenge Completion Day:   2023-07-10
 '''
 #Puzzle Part I
-# TODO: Create a matrix based on the size required
-# TODO: Create vars to flag different actions: on, off, toggle
-# TODO: Create if condition based on each different action
+# DONE: Create a matrix based on the size required
+# DONE: Create vars to flag different actions: on, off, toggle
+# DONE: Create if condition based on each different actions
 
 #Defining grid vars
 grid = list
@@ -38,18 +38,26 @@ light_action = str
 grid_action_start = list
 grid_action_end = list
 instruction = list
+instruction_number = int
+total_lit_lights = int
 
 light_action = ""
 grid_action_start = []
 grid_action_end = []
 instruction = []
+instruction_number = 0
+total_lit_lights = 0
 
 #Mounting the Grid and Setting all lighs off
-row = 6
-column = 6
-grid = [[column for column in range(column)] for row in range(row)]
-for row in range(row):
-    for column in range(column):
+row = 999
+column = 999
+grid = [[column for column in range(column+1)] for row in range(row+1)]
+for row in range(0,row + 1):
+    if row >= len(grid):
+        break
+    for column in range(0,column + 1):
+        if column >= len(grid):
+            break
         grid[row][column] = 0
         column += 1
     row += 1
@@ -57,54 +65,80 @@ print(f'Starting the grid...')
 print(grid)
 
 #Executing instructions
+
+#Resetting row and column values to zero
+row = 0
+column = 0
 with open("input/2015-Day06-input.txt") as f:
-    print(f"---------------------------------")
-    #Reading each line of the file
-    instruction = f.readline()
-    print(f'Executing the following instruction:')
-    print(instruction)
-    instruction = instruction.split(" ")
+    while True:
+        #Reading each line of the file
+        instruction = f.readline().strip()
+        instruction_number += 1
+        if not instruction:
+            break
 
-    #Normalizing list based on readline
-    if instruction[0] == "turn" and instruction[1] == "on":
-        instruction[0] = "turn on"
-        instruction.pop(1)
-    if instruction[0] == "turn" and instruction[1] == "off":
-        instruction[0] = "turn off"
-        instruction.pop(1)
+        instruction = list(instruction.split(" "))
+        print(f'Executing instruction #{instruction_number}:')
+        print(instruction)
 
-    #Normalizing grid instructions
-    light_action = instruction[0]
-    grid_action_start = instruction[1].split(",")
-    grid_action_end = instruction[3].split(",")
-    grid_row_start = int(grid_action_start[0])
-    grid_row_end = int(grid_action_end[0])
-    grid_column_start = int(grid_action_start[1])
-    grid_column_end = int(grid_action_end[1])
+        #Normalizing the list based on instruction in readline
+        if instruction[0] == "turn" and instruction[1] == "on":
+            instruction[0] = "turn on"
+            instruction.pop(1)
+        if instruction[0] == "turn" and instruction[1] == "off":
+            instruction[0] = "turn off"
+            instruction.pop(1)
 
-    #row = 0
-    #column = 0
-    for row in range(grid_row_start,grid_row_end+1):
-        for column in range(grid_column_start,grid_column_end+1):
-            #print(f'Grid Row {row}{column} = {grid[row][column]}')
-            #TODO: Create IFs for light action, but first refine action for "turn on" and "turn off"
+        #Normalizing grid instructions
+        light_action = instruction[0]
+        grid_action_start = instruction[1].split(",")
+        grid_action_end = instruction[3].split(",")
+        grid_row_start = int(grid_action_start[0])
+        grid_row_end = int(grid_action_end[0])
+        grid_column_start = int(grid_action_start[1])
+        grid_column_end = int(grid_action_end[1])
 
-            #Executing taking actions on each light on the grid
-            print(f'Executing light instructions...')
-            if light_action == "turn on":
-                print(f'Turning on light: {row}{column}')
-                grid[row][column] = 1
-            if light_action == "turn off":
-                print(f'Turning off light: {row}{column}')
-                grid[row][column] = 0
-            if light_action == "toggle":
-                print(f'Toggling light: {row}{column}')
-                if grid[row][column] == 0:
+        row = 0
+        column = 0
+        for row in range(grid_row_start,grid_row_end+1):
+            if row > grid_row_end+1:
+                break
+            for column in range(grid_column_start,grid_column_end+1):
+                if column > grid_column_end+1:
+                    break
+
+                #Taking actions on each light on the grid
+                #[DEBUG ONLY] print(f'Executing light instructions...')
+                if light_action == "turn on":
+                    #[DEBUG ONLY] print(f'Turning on light: {row}{column}')
                     grid[row][column] = 1
-                elif grid[row][column] == 1:
+                if light_action == "turn off":
+                    #[DEBUG ONLY] print(f'Turning off light: {row}{column}')
                     grid[row][column] = 0
-            column += 1
-        row += 1
-        print(grid)
-    print(f"Moving to the next instruction...")
-    print(f"---------------------------------")
+                if light_action == "toggle":
+                    #[DEBUG ONLY]print(f'Toggling light: {row}{column}')
+                    if grid[row][column] == 0:
+                        grid[row][column] = 1
+                    elif grid[row][column] == 1:
+                        grid[row][column] = 0
+                column += 1
+            column = grid_column_start
+            row += 1
+        print(f"Moving to the next instruction...")
+        print(f"---------------------------------")
+print(f"End of processing.")
+
+#Counting number of lights that are lit
+row = 999
+column = 999
+for row in range(0,row + 1):
+    if row == len(grid):
+        break
+    for column in range(0,column + 1):
+        if column == len(grid):
+            break
+        if grid[row][column] == 1:
+            total_lit_lights += 1
+        column += 1
+    row += 1
+print(f"The total number of lights lit are: {total_lit_lights}")
